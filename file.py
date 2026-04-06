1a04b97f14064c5d184f3bd7de3a797f4ff620f6
davisimoncel0 <davisimoncelo@hotmail.com>
2026-04-06 08:27:11 -0300

    first feature

---
+++ file.py
@@ -0,0 +1,12 @@
+def boas_vindas(nome):
+    print(f'Olá, {nome}! Seja bem-vindo!')
+
+nome_digitado = input("Digite seu nome: ")
+boas_vindas(nome_digitado)
+
+def soma(num_a, num_b):
+    soma = num_a + num_b
+    return soma
+
+resultado_soma = soma(num_a=5, num_b=3)
+print(soma(num_a=17, num_b=8))
